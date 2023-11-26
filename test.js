27fbcf7f126db50c780f0e5451160a6d25f7951f
Ayi <58356542+Ayi1337@users.noreply.github.com>
2023-11-26 08:10:18 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
+let url = $request.url;
 let response = JSON.parse($response.body);
 
-// 修改字段值
-if (response.hasOwnProperty("withholdMoney")) {
-    response["withholdMoney"] = 0;
-}
-if (response.hasOwnProperty("preDeductMoneySend")) {
-    response["preDeductMoneySend"] = "0";
-}
-if (response.hasOwnProperty("preDeductMoney")) {
-    response["preDeductMoney"] = "0";
+// 检查URL并相应地修改响应体
+if (url.indexOf('/order/downRate/bluetooth/rateOrder') !== -1 || url.indexOf('/device/smallType/id') !== -1) {
+    // 修改字段值
+    if (response.hasOwnProperty("withholdMoney")) {
+        response["withholdMoney"] = 0;
+    }
+    if (response.hasOwnProperty("preDeductMoneySend")) {
+        response["preDeductMoneySend"] = "0";
+    }
+    if (response.hasOwnProperty("preDeductMoney")) {
+        response["preDeductMoney"] = "0";
+    }
 }
 
 $done({body: JSON.stringify(response)});
